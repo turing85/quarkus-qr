d51e5a27c8ff9cd41b4f75bc9875bcd76590e2c6
Marco Bungart <marco.bungart@googlemail.com>
2023-06-17 05:42:23 +0200

    Inject actor into step

--- src/test/java/de/turing85/qr/code/generator/steps/Http.java
+++ src/test/java/de/turing85/qr/code/generator/steps/Http.java
@@ -3,15 +3,18 @@ package de.turing85.qr.code.generator.steps;
 import java.io.IOException;
 
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 
 import com.google.zxing.NotFoundException;
 import de.turing85.qr.code.generator.actor.QrCodeActor;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import lombok.RequiredArgsConstructor;
 
 @ApplicationScoped
+@RequiredArgsConstructor(onConstructor_ = {@Inject})
 public class Http {
-  private final QrCodeActor actor = new QrCodeActor();
+  private final QrCodeActor actor;
 
   @When("I get {string} as QR-code")
   public void whenIAccessEndpoint(String text) {
