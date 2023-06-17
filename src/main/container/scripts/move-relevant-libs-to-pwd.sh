fb863a4df6d14f1ab048c4a5d4151d64225a923e
Marco Bungart <marco.bungart@googlemail.com>
2023-06-17 12:44:15 +0200

    Overhaul container builds
    - Restructurize keep-relevant-libs.sh
    - Rename keep-relevant-libs.sh to move-relevant-libs-to-pwd.sh
    - Parameterize Containerfile.native-distroless and Containerfile.temurin
    - Name all stages in all containerfiles
    - Update to latest tag-versions
    - Reference images by tag and digest

--- src/main/container/scripts/move-relevant-libs-to-pwd.sh
+++ src/main/container/scripts/move-relevant-libs-to-pwd.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
+function relevant_libs() {
+  local relevant_libs=( \
+    'libbz2' \
+    'libfreetype' \
+    'libpng16' \
+  )
+  echo "${relevant_libs[*]}"
+}
+
+function relevant_libs_regex() {
+  echo ".*($(IFS='|'; echo "$(relevant_libs)"))\.so.*"
+}
+
+
 function copy_lib64_to_pwd() {
   cp \
    --no-dereference \
@@ -17,11 +31,12 @@ function files_in_pwd() {
   echo "${files[*]}"
 }
 
-function files_in_pwd_to_keep() {
+function libs_in_pwd_to_keep() {
   local files_to_keep=()
+  local relevant_libs_regex="$(relevant_libs_regex)"
   for file in "${@}"
   do
-    if [[ "${file}" =~ .*(libfreetype|libbz2|libpng16)\.so.* ]]
+    if [[ "${file}" =~ ${relevant_libs_regex} ]]
     then
       files_to_keep+=( "${file}" )
     fi
@@ -29,21 +44,21 @@ function files_in_pwd_to_keep() {
   echo "${files_to_keep[*]}"
 }
 
-function remove_unwanted_files_from_pwd() {
+function remove_unwanted_libs_from_pwd() {
   local files=()
   mapfile \
     -d ' ' `# set delimiter` \
     -t `# truncate trailing delimiter` \
     files < <(files_in_pwd)
-  local files_to_keep=()
+  local libs_to_keep=()
   mapfile \
     -d ' ' `# set delimiter` \
     -t `# truncate trailing delimiter` \
-    files_to_keep < <(files_in_pwd_to_keep "${files[@]}")
+    libs_to_keep < <(libs_in_pwd_to_keep "${files[@]}")
   for file in "${files[@]}"
   do
     file="${file%%[[:space:]]}"
-    if [[ ! "${files_to_keep[*]}" =~ (^|.*[[:space:]]+)"${file}"([[:space:]]+.*|$).* ]]
+    if [[ ! "${libs_to_keep[*]}" =~ (^|.*[[:space:]]+)"${file}"([[:space:]]+.*|$).* ]]
     then
       rm -rf "${file}"
     fi
@@ -52,7 +67,7 @@ function remove_unwanted_files_from_pwd() {
 
 function run() {
   copy_lib64_to_pwd
-  remove_unwanted_files_from_pwd
+  remove_unwanted_libs_from_pwd
 }
 
 run
