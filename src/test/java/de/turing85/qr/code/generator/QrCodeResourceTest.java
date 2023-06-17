04c1741b1a28d8c2a5f6ce63b20c60563c579c7a
Marco Bungart <marco.bungart@googlemail.com>
2023-06-17 05:08:38 +0200

    Better setup for test, especially a nicer weld-setup

--- src/test/java/de/turing85/qr/code/generator/QrCodeResourceTest.java
+++ src/test/java/de/turing85/qr/code/generator/QrCodeResourceTest.java
@@ -1,7 +1,10 @@
 package de.turing85.qr.code.generator;
 
+import io.quarkiverse.cucumber.CucumberOptions;
 import io.quarkiverse.cucumber.CucumberQuarkusTest;
 
+@CucumberOptions(features = {"src/test/resources/de/turing85/qr/code/generator/qrCode.feature"},
+    glue = "de.turing85.qr.code.generator.steps")
 public class QrCodeResourceTest extends CucumberQuarkusTest {
   public static void main(String... args) {
     runMain(QrCodeResourceTest.class, args);
