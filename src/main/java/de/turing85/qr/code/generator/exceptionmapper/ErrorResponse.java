be5b420634d77dc616d318eae4970890c3410c06
Marco Bungart <marco.bungart@googlemail.com>
2025-02-14 17:28:56 +0100

    Fix code format

--- src/main/java/de/turing85/qr/code/generator/exceptionmapper/ErrorResponse.java
+++ src/main/java/de/turing85/qr/code/generator/exceptionmapper/ErrorResponse.java
@@ -2,6 +2,4 @@ package de.turing85.qr.code.generator.exceptionmapper;
 
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
-@RegisterForReflection
-public record ErrorResponse(String message) {
-}
+@RegisterForReflection public record ErrorResponse(String message){}
